dec6e164cc4bfa38f279372b36aebcdf432de4ec
Anamul Haque <anamulhaque4631@gmail.com>
2024-09-23 12:32:46 +0600

    environment-setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo eum atque eligendi quos. Ut tempore, rem eaque, architecto, dolorem beatae atque error ad qui placeat voluptates fugiat! Eum aut officiis temporibus? Quibusdam officia suscipit veniam sequi qui aperiam ullam fugiat, quo a deserunt totam at! Temporibus ab doloremque veritatis qui minima placeat earum aut at quo odio commodi, dolores sit illum ipsa aliquam hic ad dicta dolorem nam veniam. Accusamus maiores vitae porro quos perferendis! Assumenda pariatur, saepe quasi ad aperiam dignissimos blanditiis. Eligendi quidem expedita alias qui repellendus atque minima suscipit quam. Nisi possimus unde deserunt corporis qui nam.</p>
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default App
